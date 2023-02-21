79c061ed0a47a83d6c64a13e8a82fd37adea7c05
Ángel <angelmonclova@gmail.com>
2023-02-21 19:08:17 +0100

    Add more config

--- app.ts
+++ app.ts
@@ -1,11 +1,16 @@
-import express from 'express';
-const app = express();
-const port = 3000;
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/indent */
+import express from 'express'
+const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+  res.send('Hello World!')
+})
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+// eslint-disable-next-line @typescript-eslint/semi
+app.listen(port, () => `Example app listening on port ${port}`)
+
+app.post('/', (req, res) => {
+  res.send('Hello new World!')
+})
--- build/app.js
+++ build/app.js
@@ -1,1 +1,13 @@
-"use strict";
+/* eslint-disable @typescript-eslint/semi */
+/* eslint-disable @typescript-eslint/indent */
+import express from 'express';
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+// eslint-disable-next-line @typescript-eslint/semi
+app.listen(port, () => `Example app listening on port ${port}`);
+app.post('/', (req, res) => {
+    res.send('Hello new World!');
+});
